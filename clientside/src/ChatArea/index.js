1c0d26b76ee183fee196f034b85bf8282e9c21fc
ashishCloudesign <ashish.saini@cloudesign.com>
2024-06-03 01:55:31 +0530

    socket working fine

--- clientside/src/ChatArea/index.js
+++ clientside/src/ChatArea/index.js
@@ -1,57 +1,65 @@
 import { io } from "socket.io-client";
 import "./index.css";
-import react from "react";
+import react, { useEffect, useState } from "react";
 
-export default function chatArea(props) {
+export default function ChatArea(props) {
   const form = document.querySelector("form");
   const input = document.querySelector(".input");
-  const messages = document.querySelector(".messages");
-  const username = prompt("Please enter a nickname: ", "");
-  const socket = io();
+  // const messages = document.querySelector(".messages");
+  const username = "Ashish"; //prompt("Please enter a nickname: ", "");
 
-  form.addEventListener(
-    "submit",
-    function (event) {
-      event.preventDefault();
+  const [messages, setMessages] = useState([]);
+  const room = 1;
 
-      addMessage(username + ": " + input.value);
+  useEffect(() => {
+    console.log("ran");
+    if (window.socket) return;
+    const socket = io("http://localhost:3001", {
+      reconnectionAttempts: 0,
+      reconnection: false,
+    });
 
-      socket.emit("chat_message", {
-        message: input.value,
+    socket.on("connect", () => {
+      console.log(socket.id);
+      // addMessage("You have joined the chat as '" + username + "'.");
+      // socket.emit("user_join", username);
+      socket.emit("joinRoom", { username, room });
+
+      socket.on("message", (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
       });
+      window.socket = socket;
+    });
 
-      input.value = "";
-      return false;
-    },
-    false
-  );
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
-  socket.on("chat_message", function (data) {
-    addMessage(data.username + ": " + data.message);
-  });
+  function submit(event) {
+    console.log("clicked", input?.value);
+    event.preventDefault();
 
-  socket.on("user_join", function (data) {
-    addMessage(data + " just joined the chat!");
-  });
+    // addMessage({ user: username, text: input.value });
 
-  socket.on("user_leave", function (data) {
-    addMessage(data + " has left the chat.");
-  });
-
-  addMessage("You have joined the chat as '" + username + "'.");
-  socket.emit("user_join", username);
-
-  function addMessage(message) {
-    const li = document.createElement("li");
-    li.innerHTML = message;
-    messages.appendChild(li);
-    window.scrollTo(0, document.body.scrollHeight);
+    window.socket.emit("chatMessage", {
+      message: input.value,
+    });
   }
+
+  function addMessage(mssg) {
+    setMessages([...messages, mssg]);
+  }
+  console.log({ messages });
   return (
     <>
-      <ul class="messages"></ul>
-      <form>
-        <input type="text" class="input" autocomplete="off" autofocus />
+      <ul className="messages">
+        {messages.map((each, index) => (
+          <li key={index}>{each.text}</li>
+        ))}
+      </ul>
+      <form onSubmit={submit}>
+        <input type="text" className="input" />
         <button>Send</button>
       </form>
     </>
--- server/index.js
+++ server/index.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require("socket.io");
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-// // Serve the index.html file
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// Handle socket connections
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    // Handle incoming messages
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
-
-    // Handle disconnections
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+const io = new Server({
+  cors: "*",
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("joinRoom", ({ username, room }) => {
+    socket.join(room);
+    console.log(`${username} joined room ${room}`);
+
+    // Welcome current user
+    socket.emit("message", { text: "Welcome to the chat!" });
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(room)
+      .emit("message", { text: `${username} has joined the chat` });
+
+    // Listen for chat messages
+    socket.on("chatMessage", (msg) => {
+      io.to(room).emit("message", { user: username, text: msg.message });
+    });
+
+    // Runs when client disconnects
+    socket.on("disconnect", () => {
+      io.to(room).emit("message", { text: `${username} has left the chat` });
+      console.log("Client disconnected");
+    });
+  });
 });
+
+// // Start the server
+const PORT = process.env.PORT || 3001;
+io.listen(PORT);
